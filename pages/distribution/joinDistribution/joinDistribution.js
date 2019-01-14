eb390606d97039b80012858f56d1aaa6e7eea036
zhousong <zhousong@51zan.com>
2019-01-14 18:56:49 +0800

    分销开发中

--- pages/distribution/joinDistribution/joinDistribution.js
+++ pages/distribution/joinDistribution/joinDistribution.js
@@ -5,9 +5,20 @@ Page({
 	 * 页面的初始数据
 	 */
 	data: {
-
+		modalVisible: false
 	},
-
+	handleClick: function () {
+		this.setData({
+			modalVisible: true
+		})
+	},
+	handleCloseModal: function () {
+		this.setData({
+			modalVisible: false
+		})
+	},
+	// 阻止蒙层事件冒泡
+	handleTouchmove: function () {},
 	/**
 	 * 生命周期函数--监听页面加载
 	 */
