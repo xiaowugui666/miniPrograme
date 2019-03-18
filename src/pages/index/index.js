6c8a648ed2b02d13eddaae9f768e9fd752ec0dda
zhousong <zhousong@51zan.com>
2019-03-18 16:16:26 +0800

    店铺初始时显示装修之前数据

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -5,13 +5,30 @@ Page({
 	data: {
 		//分类
 		image: 'https://image.51zan.com/',
+		categoryList:[],
 		description: {},
+		newCate:'',
 		remain:"",
+		current:0,
+		tabSwiperArr: [],
+		currentTab: 0,
+		tabScrollTop: false,
 		keyword:'',
 		winWidth:'',
+		good:[],
+		currentPage: 0,
+		currentGroupPage: 0,
+		currentRecommendPage: 0,
+		currentSpecialPage: 0,
+
 		skinStyle: app.globalData.skinStyle,
-        pageData: [],
+        pageData: null,
 	},
+	/**
+	 * 
+	 * @description 店铺装修数据如果不为null,则执行店铺装修部分逻辑，否则执行之前逻辑
+	 */
+
 	// 获取当前数组中为商品列表模块的商品ID数据,同时根据不同模块的需求进行数据处理
     getGoodListIdsData: function (data) {
 		let goodListIdsData = [], postDataList = JSON.parse(JSON.stringify(data)), windowHeight = this.data.windowHeight * 2
@@ -59,12 +76,18 @@ Page({
         return goodListIdsData
 	},
 	// 切换tab
-	onChangeTab: function (e) {
+	onChangeTabFitment: function (e) {
 		const clickArray = e.currentTarget.dataset.value.split("_")
 		let changeArr = this.data.pageData
 		changeArr[parseFloat(clickArray[0])].currentTab = parseFloat(clickArray[1])
 		this.setData({
 			pageData: changeArr
+		})
+	},
+	onChangeTab: function (e) {
+		this.setData({
+			currentTab: e.currentTarget.dataset.value,
+			currentPage: 0
 		})
 	},
 	handleSeeMore: function (e) {
@@ -104,6 +127,68 @@ Page({
                     })
                 }
 			}
+		})
+	},
+	getInitData: function (params) {
+        const that = this
+        wx.request({
+            url: app.globalData.http + '/mpa/index/decoration_products',
+            method: 'POST',
+            header:{
+                'Api-Ext': app.globalData.apiExt
+            },
+            data: {
+                main: params
+            },
+            success(res) {
+                if (res.statusCode === 200) {
+                    let goodList = res.data.data, pageShowingArr = that.data.pageData
+                    goodList.forEach(currentItem => {
+						let typeArr = currentItem.type.split('_'), i = parseFloat(typeArr[0]), j = parseFloat(typeArr[1])
+                        pageShowingArr[i].data[j].currentShowData = pageShowingArr[i].data[j].currentShowData.concat(currentItem.data)
+                    })
+                    that.setData({
+                        pageData: pageShowingArr
+                    })
+                }
+
+            },
+            fail: function (res) {
+                console.log(res)
+            }
+        })
+	},
+	getInitElement: function () {
+		const that = this
+		return new Promise((resolve, reject) => {
+			wx.request({
+				url: app.globalData.http + '/mpa/index/decoration',
+				method: 'GET',
+				header:{
+					'Api-Ext': app.globalData.apiExt
+				},
+				success(res) {
+					if (res.statusCode === 200) {
+						if (res.data === null) {
+							that.setData({
+								pageData: res.data
+							})
+							resolve(res.data)
+						} else {
+							that.setData({
+								pageData: res.data.data
+							})
+							resolve(res.data.data)
+						}
+					} else {
+						reject(res)
+					}
+	
+				},
+				fail: function (res) {
+					reject(res)
+				}
+			})
 		})
 	},
 	// 页面加载
@@ -148,84 +233,294 @@ Page({
 			}
 		});
 		this.getInitElement().then((data) => {
-			let goodListIdsData = this.getGoodListIdsData(data)
-			this.getInitData(goodListIdsData)
+			if (data === null ) {
+				this.getData()
+			} else {
+				let goodListIdsData = this.getGoodListIdsData(data)
+				this.getInitData(goodListIdsData)
+			}
 			app.login()
+		})
+	},
+	getData () {
+		let that = this;
+		//商品分类
+		wx.request({
+			url: app.globalData.http + '/mpa/category',
+			method:'get',
+			header: {
+				'Api-Ext': app.globalData.apiExt
+			},
+			success:function(res){
+				var cateNum = Math.ceil(res.data.length / 5)
+				var remain = res.data.length % 5
+				var cateArr=[]
+				for(var i=0;i<cateNum;i++){
+					 cateArr.push(1)
+				}
+				that.setData({
+					categoryList: res.data,
+					newCate: cateArr,
+					remain: remain
+				})
+			}
+		})
+		this.getGroupData()
+			.then(() => this.getRecommendData())
+			.then(() => this.getSpecialData())
+			.then(() => this.getNormalData())
+	},
+	// 获取商品列表
+	getNormalData: function (params) {
+		let that = this, page = this.data.currentPage;
+		return new Promise((resolve, reject) => {
+			wx.request({
+				url: app.globalData.http + '/mpa/goods/search',
+				header: {
+					'Api-Ext': app.globalData.apiExt
+				},
+				data: {
+					page: page
+				},
+				dataType: 'json',
+				method: 'GET',
+				success: function (data) {
+					if (data.statusCode >= 200 && data.statusCode < 300) {
+						if (params === 'isConcat' && data.data.length > 0) {
+							let tempArr = that.data.good
+							let newArr = tempArr.concat(data.data)
+							that.setData({
+								good: newArr
+							})
+						} else if (params === 'isConcat' &&  data.data.length == 0) {
+							let newPage = that.data.currentPage
+							newPage--
+							that.setData({
+								currentPage: newPage
+							})
+						} else {
+							that.setData({
+								good:data.data
+							})
+						}
+						resolve()
+					} else {
+						reject()
+					}
+				},
+				fail: function (data) {
+					reject()
+				}
+			})
+		})
+	},
+	getGroupData: function (params) {
+		let that = this, page = this.data.currentGroupPage;
+		return new Promise((resolve, reject) => {
+			wx.request({
+				url: app.globalData.http + '/mpa/groupon_goods',
+				header: {
+					'Api-Ext': app.globalData.apiExt
+				},
+				data: {
+					page: page
+				},
+				dataType: 'json',
+				method: 'GET',
+				success: function (data) {
+					if (data.statusCode >= 200 && data.statusCode < 300) {
+						if (data.data && data.data.length > 0 && params === 'isConcat') {
+							let tempArr = that.data.tabSwiperArr;
+							tempArr.map(item => {
+								if (item.type === 1) {
+									item.data = item.data.concat(data.data)
+								}
+								return item
+							});
+							that.setData({
+								tabSwiperArr: tempArr
+							})
+						} else if (data.data && params === 'isConcat') {
+							let newPage = that.data.currentGroupPage
+							newPage--
+							that.setData({
+								currentGroupPage: newPage
+							})
+						} else if (data.data && data.data.length > 0) {
+							let tempArr = []
+							tempArr.push({
+								type: 1,
+								label: '超值拼团',
+								data: data.data
+							})
+							that.setData({
+								tabSwiperArr: tempArr
+							})
+						}
+						resolve()
+					} else {
+						reject()
+					}
+				},
+				fail: function (res) {
+					reject()
+				},
+			})
+		})
+	},
+	getRecommendData: function (params) {
+		let that = this, page = this.data.currentRecommendPage;		
+		return new Promise((resolve, reject) => {
+			wx.request({
+				url: app.globalData.http + '/mpa/goods/recommend',
+				method: 'GET',
+				header:{
+					'Api-Ext': app.globalData.apiExt
+				},
+				data: {
+					page: page
+				},
+				success(res) {
+					var code = res.statusCode.toString()
+					if (code.indexOf('20')>-1) {
+						if (res.data.length > 0 && params === 'isConcat') {
+							let tempArr = that.data.tabSwiperArr;
+							tempArr.map(item => {
+								if (item.type === 2) {
+									item.data = item.data.concat(res.data)									
+								}
+								return item
+							});
+							that.setData({
+								tabSwiperArr: tempArr
+							})
+						} else if (params === 'isConcat') {
+							let newPage = that.data.currentRecommendPage
+							newPage--
+							that.setData({
+								currentRecommendPage: newPage
+							})
+						} else if (res.data.length > 0) {
+							let tempArr = that.data.tabSwiperArr, hasCurrentData = false
+							for (let i = 0, leng = tempArr.length; i < leng; i++) {
+								if (tempArr[i].type == 2) {
+									hasCurrentData = true
+								}
+							}
+							if (tempArr.length == 0 || !hasCurrentData) {
+								tempArr.push({
+									type: 2,
+									label: '好物推荐',
+									data: res.data
+								})
+							}
+							that.setData({
+								tabSwiperArr: tempArr,
+							})
+						}
+						resolve()
+					} else {
+						reject()
+					}
+				},
+				fail: function (res) {
+					reject()
+				},
+			})
+		})
+	},
+	getSpecialData: function (params) {
+		let that = this, page = this.data.currentSpecialPage;		
+		return new Promise((resolve, reject) => {
+			wx.request({
+				url: app.globalData.http + '/mpa/goods/special',
+				method: 'GET',
+				header:{
+					'Api-Ext':app.globalData.apiExt
+				},
+				data: {
+					page: page
+				},
+				success(res) {
+					if (res.statusCode >= 200 && res.statusCode < 300) {
+						if (res.data.length >0 && params === 'isConcat') {
+							let tempArr = that.data.tabSwiperArr;
+							tempArr.map(item => {
+								if (item.type === 3) {
+									item.data = item.data.concat(data.data)
+								}
+								return item
+							});
+							that.setData({
+								tabSwiperArr: tempArr
+							})
+						} else if (params === 'isConcat') {
+							let newPage = that.data.currentSpecialPage
+							newPage--
+							that.setData({
+								currentSpecialPage: newPage
+							})
+						} else if (res.data.length > 0) {
+							let tempArr = that.data.tabSwiperArr, hasCurrentData = false
+							for (let i = 0, leng = tempArr.length; i < leng; i++) {
+								if (tempArr[i].type == 3) {
+									hasCurrentData = true
+								}
+							}
+							if (tempArr.length == 0 || !hasCurrentData) {
+								tempArr.push({
+									type: 3,
+									label: '精选特价',
+									data: res.data
+								})
+							}
+							that.setData({
+								tabSwiperArr: tempArr,
+							})
+						}
+						resolve()
+					} else {
+						reject()
+					}
+				},
+				fail: function (res) {
+					reject()
+				},
+			})
 		})
 	},
 	//下拉刷新
 	onPullDownRefresh: function () {
 		this.getInitElement().then((data) => {
-			let goodListIdsData = this.getGoodListIdsData(data)
-			this.getInitData(goodListIdsData)
+			if (data === null ) {
+				this.setData({
+					tabSwiperArr: [],
+					good: [],
+					currentPage: 0,
+					currentGroupPage: 0,
+					currentRecommendPage: 0,
+					currentSpecialPage: 0,
+					currentTab: 0
+				})
+		
+				this.getData()
+			} else {
+				let goodListIdsData = this.getGoodListIdsData(data)
+				this.getInitData(goodListIdsData)
+			}
 		}).then(() => {
 			wx.stopPullDownRefresh()
 		})
 		
 	},
 	phoneCall: function (e) {
-		const phoneNumber = e.currentTarget.dataset.phonenumber
+		const phoneNumber = e.currentTarget.dataset.phonenumber || app.globalData.mobile
 		wx.makePhoneCall({
 			phoneNumber: phoneNumber
 		})	  
 	},
-	getInitData: function (params) {
-        const that = this
-        wx.request({
-            url: app.globalData.http + '/mpa/index/decoration_products',
-            method: 'POST',
-            header:{
-                'Api-Ext': app.globalData.apiExt
-            },
-            data: {
-                main: params
-            },
-            success(res) {
-                if (res.statusCode === 200) {
-                    let goodList = res.data.data, pageShowingArr = that.data.pageData
-                    goodList.forEach(currentItem => {
-						let typeArr = currentItem.type.split('_'), i = parseFloat(typeArr[0]), j = parseFloat(typeArr[1])
-                        pageShowingArr[i].data[j].currentShowData = pageShowingArr[i].data[j].currentShowData.concat(currentItem.data)
-                    })
-                    that.setData({
-                        pageData: pageShowingArr
-                    })
-                }
-
-            },
-            fail: function (res) {
-                console.log(res)
-            }
-        })
-	},
-	getInitElement: function () {
-		const that = this
-		return new Promise((resolve, reject) => {
-			wx.request({
-				url: app.globalData.http + '/mpa/index/decoration',
-				method: 'GET',
-				header:{
-					'Api-Ext': app.globalData.apiExt
-				},
-				success(res) {
-					if (res.statusCode === 200) {
-						that.setData({
-							pageData: res.data.data
-						})
-						resolve(res.data.data)
-					} else {
-						reject(res)
-					}
-	
-				},
-				fail: function (res) {
-					reject(res)
-				}
-			})
-		})
-	},
 	//一级分类滚动
-	scrollCategory:function(e){
+	scrollCategoryFitment:function(e){
 		const scrollTypeArr = e.currentTarget.dataset.type.split('_'), i = scrollTypeArr[0]
 		let changedPageArr = this.data.pageData
 		var that = this
@@ -255,13 +550,28 @@ Page({
 			})
 		}
 	},
-	onChangeSwiperItem: function (e) {
-		if (e.detail.source === 'touch') {
+	scrollCategory:function(e){
+		//宽度
+		var scrollWidth = parseInt(e.detail.scrollWidth)
+		//滚动的距离
+		var scrollLeft = parseInt(e.detail.scrollLeft)
+		//屏幕的宽度
+		var width = parseInt(this.data.winWidth)
+		//剩余的分类
+		var remain = parseInt(this.data.remain)
+		var cur = Math.floor(scrollLeft / width)
+		if (cur < 0) {
+			cur = 0
+		}
+		var cateNum = Math.floor(scrollWidth / width)
+		if (remain != 0 && scrollLeft >= (scrollWidth -width-(remain-1) * width/5)) {
+			cur++
 			this.setData({
-				tabObject: {
-					...this.data.tabObject,
-					currentTab: e.detail.current
-				},
+				current: cur
+			})
+		}else{
+			this.setData({
+				current: cur
 			})
 		}
 	},
@@ -297,4 +607,49 @@ Page({
 			url: '/pages/category/category',
 		})
 	},
+	onReachBottom: function (e) {
+		const pageData = this.data.pageData
+		if (pageData !== null) return
+		wx.showLoading({
+			title: '加载中',
+		})
+		let currentTab = this.data.currentTab, tabSwiperArr = this.data.tabSwiperArr;
+		if (tabSwiperArr.length === 0) {
+			let newPage = this.data.currentPage
+			newPage++
+			this.setData({
+				currentPage: newPage
+			})
+			this.getNormalData('isConcat').then(() => wx.hideLoading())
+		} else {
+			let newPage
+			switch (tabSwiperArr[currentTab].type) {
+				case 1:
+					newPage = this.data.currentGroupPage
+					newPage++
+					this.setData({
+						currentGroupPage: newPage
+					})
+					this.getGroupData('isConcat').then(() => wx.hideLoading())
+					break
+				case 2:
+					newPage = this.data.currentRecommendPage
+					newPage++
+					this.setData({
+						currentRecommendPage: newPage
+					})
+					this.getRecommendData('isConcat').then(() => wx.hideLoading())
+					break
+				case 3:
+					newPage = this.data.currentSpecialPage
+					newPage++
+					this.setData({
+						currentSpecialPage: newPage
+					})
+					this.getSpecialData('isConcat').then(() => wx.hideLoading())
+					break
+				default:
+			}
+		}
+	}
 })
